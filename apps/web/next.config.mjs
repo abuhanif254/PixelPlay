5b4908151c336277c373ae4ce2c1c0eb21a1894e
md abu hanif mia <mohammadbitullah@gmail.com>
2026-08-17 15:51:11 +0600

    chore: revert to SWC minifier and set node memory limit to 2560MB for optimal native and JS heap balance

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -19,7 +19,6 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   productionBrowserSourceMaps: false,
-  swcMinify: false,
   experimental: {
     cpus: 1,
     memoryBasedWorkersCount: true
