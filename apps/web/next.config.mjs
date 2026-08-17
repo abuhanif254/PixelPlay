256310390a223b6ea6eb19f43ee65bd394f7f1cb
md abu hanif mia <mohammadbitullah@gmail.com>
2026-08-17 13:57:26 +0600

    chore: optimize next config to prevent cloudflare OOM errors

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -16,6 +16,9 @@ const nextConfig = {
       }
     ],
   },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  productionBrowserSourceMaps: false,
 };
 
 const withPWA = withPWAInit({
